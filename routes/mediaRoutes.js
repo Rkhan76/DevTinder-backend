89e546375128fb976e64a5a94053ca2ceb94ccaf
Rakhshan khan <rakshaankhan1@gmail.com>
2025-11-27 23:23:29 +0530

    fix the import

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const streamToMicroservice = require('../middleware/streamToMicroservice')
-const handletempMedia = require('../controllers/media/handletempmedia')
+const handletempmedia = require('../controllers/media/handletempmedia')
 const handleDeleteTempMedia = require('../controllers/media/deleteTempMedia')
 const { authMiddleware } = require('../middleware/authMiddleware')
 
-router.post('/upload',authMiddleware, streamToMicroservice, handletempMedia)
+router.post('/upload',authMiddleware, streamToMicroservice, handletempmedia)
 router.delete('/temp/:id', authMiddleware, handleDeleteTempMedia)
 
 module.exports = router
