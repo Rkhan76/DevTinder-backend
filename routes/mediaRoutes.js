a3efb8cf83d3e3aa0d0b52797ec2483daaca8411
Rakhshan khan <rakshaankhan1@gmail.com>
2025-11-27 23:08:49 +0530

    fix: the file name second time

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const streamToMicroservice = require('../middleware/streamToMicroservice')
-const handleTempMedia = require('../controllers/media/handletempmedia')
+const handletempMedia = require('../controllers/media/handletempmedia')
 const handleDeleteTempMedia = require('../controllers/media/deleteTempMedia')
 const { authMiddleware } = require('../middleware/authMiddleware')
 
-router.post('/upload',authMiddleware, streamToMicroservice, handleTempMedia)
+router.post('/upload',authMiddleware, streamToMicroservice, handletempMedia)
 router.delete('/temp/:id', authMiddleware, handleDeleteTempMedia)
 
 module.exports = router
